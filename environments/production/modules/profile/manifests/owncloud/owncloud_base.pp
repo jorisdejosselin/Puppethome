cb0f30bf9b38a90c5b7a302333b43c755f5c9b65
Joris <j.j.dejosselindejong@gmail.com>
2018-07-11 12:20:53 +0200

    Remove white spacing

--- environments/production/modules/profile/manifests/owncloud/owncloud_base.pp
+++ environments/production/modules/profile/manifests/owncloud/owncloud_base.pp
@@ -1,6 +1,6 @@
 # Voor mij owncloud server
 class profile::owncloud::owncloud_base {
-        firewall { '100 allow http and https access':
+    firewall { '100 allow http and https access':
       dport  => [80, 443],
       proto  => tcp,
       action => accept,
