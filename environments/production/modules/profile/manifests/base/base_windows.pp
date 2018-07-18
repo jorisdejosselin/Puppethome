e1a9e19feb4cb0edc644e4877486b7b8f5810aaa
Joris <j.j.dejosselindejong@gmail.com>
2018-07-18 16:57:08 +0200

    add comment

--- environments/production/modules/profile/manifests/base/base_windows.pp
+++ environments/production/modules/profile/manifests/base/base_windows.pp
@@ -1,3 +1,4 @@
+#documented
 class profile::base::base_windows (
   $pack_windows      =  lookup('profile::base::base_windows::pack_windows', {value_type  =>  Tuple,  default_value  =>  ['notepadplusplus','puppet-agent','qbittorrent','winscp','chocolatey','putty','curl']}),
   ){
