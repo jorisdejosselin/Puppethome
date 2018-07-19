51f6ffd621bded3d30a33f3233cea9b0df5b0694
Joris <j.j.dejosselindejong@gmail.com>
2018-07-19 08:43:04 +0200

    Update nginx.pp

--- environments/production/modules/profile/manifests/nginx/nginx_base.pp
+++ environments/production/modules/profile/manifests/nginx/nginx_base.pp
@@ -5,7 +5,7 @@ class profile::nginx::nginx_base {
       proto  => tcp,
       action => accept,
         }
-    class{'nginx':
+    class {'nginx':
       manage_repo    => true,
       package_source => 'nginx-mainline'
     }
@@ -13,11 +13,11 @@ class profile::nginx::nginx_base {
       email => 'j.j.dejosselindejong@gmail.com',
     }
     letsencrypt::certonly { 'Apply certs':
-      domains         => ['jorisdejosselindejong.nl', 'owncloud.jorisdejosselindejong.nl'],
-      manage_cron       => true,
-      cron_before_command   => 'systemctl stop nginx',
-      cron_success_command   => 'systemctl restart nginx',
-      suppress_cron_output   => true,
+      domains              => ['jorisdejosselindejong.nl', 'owncloud.jorisdejosselindejong.nl'],
+      manage_cron          => true,
+      cron_before_command  => 'systemctl stop nginx',
+      cron_success_command => 'systemctl restart nginx',
+      suppress_cron_output => true,
     }
     nginx::resource::server { 'test.jorisdejosselindejong.nl':
       listen_port => 80,
@@ -37,10 +37,10 @@ class profile::nginx::nginx_base {
       command     => 'systemctl daemon-reload',
       path        => [ '/usr/bin', '/bin', '/usr/sbin' ],
       refreshonly => true,
-      require     => file['/lib/systemd/system/phpscript.service'],
+      require     => File['/lib/systemd/system/phpscript.service'],
     }
     service { 'phpscript':
       ensure  => running,
-      require => exec['myservice-systemd-reload'],
+      require => Exec['myservice-systemd-reload'],
     }
 }
