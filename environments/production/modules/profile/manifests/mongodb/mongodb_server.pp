0a0a5a054f0e2f938e95622352316c1c99bc1ff4
Joris <j.j.dejosselindejong@gmail.com>
2018-07-24 13:48:00 +0200

    Add firewall rules

--- environments/production/modules/profile/manifests/mongodb/mongodb_server.pp
+++ environments/production/modules/profile/manifests/mongodb/mongodb_server.pp
@@ -8,4 +8,22 @@ class profile::mongodb::mongodb_server (
       user          => 'joris',
       password_hash => '0b5c29832d4a421fe29295bd99ff1379',
     }
+  firewall { '001 Allow inbound SSH':
+    dport  => 22,
+    proto  => tcp,
+    action => accept,
+  }
+  firewall { '002 Allow inbound Mongo':
+    dport  => 27018,
+    proto  => tcp,
+    action => accept,
+  }
+  firewall { '003 Allow inbound Mongo':
+    dport  => 27018,
+    proto  => udp,
+    action => accept,
+  }
+  firewall { '999 drop all other requests':
+    action => 'drop',
+  }
 }
