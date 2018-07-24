c7bc90b8df9241a29acd8c4a5e8def0e212e532e
Joris <j.j.dejosselindejong@gmail.com>
2018-07-24 13:51:12 +0200

    Add iptables tools

--- environments/production/modules/profile/manifests/base/base_linux.pp
+++ environments/production/modules/profile/manifests/base/base_linux.pp
@@ -1,7 +1,7 @@
 class profile::base::base_linux (
   $pack_RHEL      =  lookup('profile::base::base_linux::pack_RHEL', {value_type  =>  Tuple,  default_value  =>  ['nano','vim','epel-release']}),
-  $pack_RHEL_after  =  lookup('profile::base::base_linux::pack_RHEL', {value_type  =>  Tuple,  default_value  =>  ['htop','python','cifs-utils','nfs-utils','sshpass','git']}),
-  $pack_DEB      =  lookup('profile::base::base_linux::pack_DEB', {value_type  =>  Tuple,  default_value  =>  ['vim','nano','openssh-server','python','cifs-utils','sshpass','git']}),
+  $pack_RHEL_after  =  lookup('profile::base::base_linux::pack_RHEL', {value_type  =>  Tuple,  default_value  =>  ['htop','python','cifs-utils','nfs-utils','sshpass','git','iptables-services']}),
+  $pack_DEB      =  lookup('profile::base::base_linux::pack_DEB', {value_type  =>  Tuple,  default_value  =>  ['vim','nano','openssh-server','python','cifs-utils','sshpass','git','iptables-services']}),
   ){
   file { '/usr/bin/puppet':
     ensure  =>  link,
