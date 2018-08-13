1666f46e85c2bc69e3f3c54398ea69c1647afacc
Joris <j.j.dejosselindejong@gmail.com>
2018-08-13 22:44:45 +0200

    remove network stuff

--- environments/production/modules/role/manifests/openstack/openstackall.pp
+++ environments/production/modules/role/manifests/openstack/openstackall.pp
@@ -5,6 +5,5 @@ class role::openstack::openstackall {
   include ::profile::openstack::base
   include ::openstack::role::controller
   include ::openstack::role::compute
-  include ::openstack::role::network
   include ::openstack::role::storage
 }
