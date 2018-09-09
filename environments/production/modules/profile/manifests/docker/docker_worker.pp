d410c226010afc9693fa5172bff582727ad43b0b
Joris <j.j.dejosselindejong@gmail.com>
2018-09-09 20:18:26 +0200

    update docker worker with image

--- environments/production/modules/profile/manifests/docker/docker_worker.pp
+++ environments/production/modules/profile/manifests/docker/docker_worker.pp
@@ -17,6 +17,14 @@ class profile::docker::docker_worker (
   firewall { '999 drop all other requests':
     action => 'drop',
   }
+  docker::image { $imgfront:
+    ensure      =>  'latest',
+    docker_file =>  '/mnt/data/dockerfiles/pythonwebserv_front/Dockerfile',
+  }
+  docker::image { $imgback:
+    ensure      =>  'latest',
+    docker_file =>  '/mnt/data/dockerfiles/pythonwebserv_back/Dockerfile',
+  }
   include 'docker'
   docker::swarm {'cluster_worker':
     join           => true,
