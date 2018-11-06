1ffee41c37f888713fb44aaee1d4793cf6a34f5c
joris <j.j.dejosselindejong@gmail.com>
2018-11-06 14:36:32 +0100

    update puppet

--- environments/production/modules/profile/manifests/nginx/nginx_base.pp
+++ environments/production/modules/profile/manifests/nginx/nginx_base.pp
@@ -8,81 +8,11 @@ class profile::nginx::nginx_base {
   class {'nginx':
     manage_repo    => true,
     package_source => 'nginx-mainline'
+  } -> file { '/etc/nginx/nginx.conf':
+    ensure => file,
+    source => template('profile/nginx/nginx.conf.erb');
   }
-  nginx::resource::server { "jorisdejosselindejong.nl":
-    ensure              => present,
-    listen_port         => 443,
-    proxy               => 'http://192.168.178.101:3000',
-    ssl                 => true,
-    index_files         => undef,
-    ssl_cert            => '/etc/letsencrypt/live/jorisdejosselindejong.nl/fullchain.pem',
-    ssl_key             => '/etc/letsencrypt/live/jorisdejosselindejong.nl/privkey.pem',
-    location_cfg_append => { 'rewrite' => '^ https://$server_name$request_uri? permanent' },
-  }
-  nginx::resource::server { "owncloud.jorisdejosselindejong.nl":
-    ensure                => present,
-    listen_port           => 443,
-    proxy                 => 'http://192.168.178.101:60000',
-    ssl                   => true,
-    ssl_cert              => '/etc/letsencrypt/live/jorisdejosselindejong.nl/fullchain.pem',
-    ssl_key               => '/etc/letsencrypt/live/jorisdejosselindejong.nl/privkey.pem',
-    location_cfg_append => { 'rewrite' => '^ https://$server_name$request_uri? permanent' },
-  }
-  nginx::resource::server { "monitoring.jorisdejosselindejong.nl":
-    ensure                => present,
-    listen_port           => 443,
-    proxy                 => 'http://192.168.178.101:19999',
-    ssl                   => true,
-    ssl_cert              => '/etc/letsencrypt/live/jorisdejosselindejong.nl/fullchain.pem',
-    ssl_key               => '/etc/letsencrypt/live/jorisdejosselindejong.nl/privkey.pem',
-    location_cfg_append => { 'rewrite' => '^ https://$server_name$request_uri? permanent' },
-  }
-  nginx::resource::upstream { 'cloud':
-  members => [
-    '192.168.178.118:80',
-    '192.168.178.117:80',
-  ],
-}
-  nginx::resource::server { "cloud.jorisdejosselindejong.nl":
-    ensure                => present,
-    listen_port           => 443,
-    proxy                 => 'http://cloud',
-    ssl                   => true,
-    ssl_cert              => '/etc/letsencrypt/live/jorisdejosselindejong.nl/fullchain.pem',
-    ssl_key               => '/etc/letsencrypt/live/jorisdejosselindejong.nl/privkey.pem',
-    location_cfg_append => { 'rewrite' => '^ https://$server_name$request_uri? permanent' },
-  }
-  nginx::resource::server { "joict.nl":
-    ensure                => present,
-    listen_port           => 443,
-    index_files           => undef,
-    proxy                 => 'http://192.168.178.101:3000',
-    ssl                   => true,
-    ssl_cert              => '/etc/letsencrypt/live/joict.nl/fullchain.pem',
-    ssl_key               => '/etc/letsencrypt/live/joict.nl/privkey.pem',
-  }
-  nginx::resource::server { "owncloud.joict.nl":
-    ensure                => present,
-    listen_port           => 443,
-    proxy                 => 'http://192.168.178.101:60000',
-    ssl                   => true,
-    ssl_cert              => '/etc/letsencrypt/live/joict.nl/fullchain.pem',
-    ssl_key               => '/etc/letsencrypt/live/joict.nl/privkey.pem',
-  }
-  nginx::resource::server { "monitoring.joict.nl":
-    ensure                => present,
-    listen_port           => 443,
-    proxy                 => 'http://192.168.178.101:19999',
-    ssl                   => true,
-    ssl_cert              => '/etc/letsencrypt/live/joict.nl/fullchain.pem',
-    ssl_key               => '/etc/letsencrypt/live/joict.nl/privkey.pem',
-  }
-  nginx::resource::server { "cloud.joict.nl":
-    ensure                => present,
-    listen_port           => 443,
-    proxy                 => 'http://cloud',
-    ssl                   => true,
-    ssl_cert              => '/etc/letsencrypt/live/joict.nl/fullchain.pem',
-    ssl_key               => '/etc/letsencrypt/live/joict.nl/privkey.pem',
+  package { 'certbot':
+    ensure => installed,
   }
 }
