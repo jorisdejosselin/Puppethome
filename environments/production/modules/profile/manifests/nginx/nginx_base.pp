3c3012416fcf2dacb963fd3cb3083c73f9e4ba49
joris <j.j.dejosselindejong@gmail.com>
2018-11-06 14:40:07 +0100

    update proxy

--- environments/production/modules/profile/manifests/nginx/nginx_base.pp
+++ environments/production/modules/profile/manifests/nginx/nginx_base.pp
@@ -5,14 +5,14 @@ class profile::nginx::nginx_base {
     proto  => tcp,
     action => accept,
       }
-  class {'nginx':
-    manage_repo    => true,
-    package_source => 'nginx-mainline'
-  } -> file { '/etc/nginx/nginx.conf':
+  file { '/etc/nginx/nginx.conf':
     ensure => file,
     source => template('profile/nginx/nginx.conf.erb');
   }
   package { 'certbot':
+    ensure => installed,
+  }
+  package { 'nginx':
     ensure => installed,
   }
 }
